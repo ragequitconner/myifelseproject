1fa2fcb3641a00aca5c66fa82288b909dc397204
ragequitconner <>
2021-05-04 13:52:10 +0000

    added read me

---
+++ README.md
@@ -0,0 +1,1 @@
+this is a little if else project becaue I know little about bash so I want to try if else
--- main.sh
+++ main.sh
@@ -1,1 +1,6 @@
 echo Hello World
+$x = echo hi
+if $x = echo hi
+ then echo echo hi
+else echo its not 10
+bash main.sh
