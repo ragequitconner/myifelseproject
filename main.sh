6ab54b828787669eca71aca4e3f8b42bce58e911
ragequitconner <>
2021-05-05 16:56:04 +0000

    tried something and added the example code I followed/stole

--- main.sh
+++ main.sh
@@ -1,14 +1,8 @@
 
 
-echo -n "Enter a number: "
+echo -n 10+10:
 read VAR
-
-if [[ $VAR -gt 10 ]]
-then
-  echo "The variable is greater than 10."
+if [$VAR -gt 20]
+then echo "correct your not dumb"
+else echo "incorrect you are dumb"
 fi
-#echo -n what is 10+10
-#read VAR
-#if[[$VAR -gt 20]]
-#then echo "correct your not dumb"
-#else echo "incorrect you are dumb"
