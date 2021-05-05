781f27d07aab05e69df437eb4ad5f48128c48742
ragequitconner <>
2021-05-05 16:45:17 +0000

    non working code and got rid of .replit because its annoying

--- main.sh
+++ main.sh
@@ -1,8 +1,14 @@
 
-  
-if [$UID  -eq 0 ]
-  then 
-  echo you are root
 
-else echo you are not root
+echo -n "Enter a number: "
+read VAR
+
+if [[ $VAR -gt 10 ]]
+then
+  echo "The variable is greater than 10."
 fi
+#echo -n what is 10+10
+#read VAR
+#if[[$VAR -gt 20]]
+#then echo "correct your not dumb"
+#else echo "incorrect you are dumb"
